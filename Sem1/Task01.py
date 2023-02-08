8d95abbf136dc3b7aefca5babbbad73d7063e020
Monte_Kristo_VM <monte_kristo2003@mail.ru>
2023-02-08 20:53:24 +0300

    Task 01 completed

--- Sem1/Task01.py
+++ Sem1/Task01.py
@@ -1,4 +1,4 @@
-number = int(input('Введите трехзначное число: '))
+number = abs(int(input('Введите трехзначное число: ')))
 if 99 < number < 1000:
     print(f'Сумма цифр введенного числа = {number%10+number//10%10+number//100%10}')
 else:
